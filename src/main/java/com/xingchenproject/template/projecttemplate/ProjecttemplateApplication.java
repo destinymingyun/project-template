4dfccaf4e58376dbd72019c2f1a8dc02dd47743f
destinymingyun <43107868+destinymingyun@users.noreply.github.com>
2021-01-29 00:03:16 +0800

    v1.0.0 一个基础的Spring-Boot项目

---
+++ src/main/java/com/xingchenproject/template/projecttemplate/ProjecttemplateApplication.java
@@ -0,0 +1,13 @@
+package com.xingchenproject.template.projecttemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjecttemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjecttemplateApplication.class, args);
+    }
+
+}
