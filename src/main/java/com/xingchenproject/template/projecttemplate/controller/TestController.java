6a1e0f390edb042756522a83b82e8734d414aeea
destinymingyun <43107868+destinymingyun@users.noreply.github.com>
2021-01-29 00:26:09 +0800

    v1.0.1 现在整合了Spring-Mvc了，并且可以正常使用了

---
+++ src/main/java/com/xingchenproject/template/projecttemplate/controller/TestController.java
@@ -0,0 +1,24 @@
+package com.xingchenproject.template.projecttemplate.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * 测试使用Controller
+ */
+@Controller
+@RequestMapping("/test")
+public class TestController {
+
+    /**
+     * 测试Controller
+     * @return 返回字符串
+     */
+    @GetMapping("/hello")
+    @ResponseBody
+    public String helloSpringMvc() {
+        return "hello spring-mvc";
+    }
+}
